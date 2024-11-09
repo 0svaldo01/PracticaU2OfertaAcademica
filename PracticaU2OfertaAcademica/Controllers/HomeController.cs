d3a61c611cbcca08cf4182c3ea9747f73c88dfef
VICTOR OSVALDO RODRIGUEZ HIDALGO <201G0283@rcarbonifera.tecnm.mx>
2024-11-08 23:06:26 -0600

    terminado

--- PracticaU2OfertaAcademica/Controllers/HomeController.cs
+++ PracticaU2OfertaAcademica/Controllers/HomeController.cs
@@ -1,25 +1,49 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using PracticaU2OfertaAcademica.Models;
+using PracticaU2OfertaAcademica.Models.ViewModels;
 
 namespace PracticaU2OfertaAcademica.Controllers
 {
-    public class HomeController : Controller
-    {
-        MapaCurricularContext context = new();
-        public IActionResult Index()
-        {
-            var datos = context.Carreras.OrderBy(x => x.Nombre);
-            return View(datos);
-        }
-        [Route("/{Carrera}/Info")]
-        public IActionResult Info(string Carrera) 
-        {
-            var datos = context.Carreras.FirstOrDefault(x=>x.Nombre == Carrera);
-            if (datos == null)
-            {
-                return RedirectToAction("Index");
-            }
-            return View(datos);
-        }
-    }
+	public class HomeController : Controller
+	{
+		MapaCurricularContext context = new();
+		public IActionResult Index()
+		{
+			var datos = context.Carreras.OrderBy(x => x.Nombre);
+			return View(datos);
+		}
+		[Route("/{Carrera}/Info")]
+		public IActionResult Info(string Carrera)
+		{
+			var datos = context.Carreras.FirstOrDefault(x => x.Nombre == Carrera.Replace("-", " "));
+			if (datos == null)
+			{
+				return RedirectToAction("Index");
+			}
+			return View(datos);
+		}
+
+		[Route("/{Carrera}/Mapa")]
+		public IActionResult Mapa(string Carrera)
+		{
+			MapaCurricularViewModel vm = new();
+			var datos = context.Carreras.Include(x => x.Materias).FirstOrDefault(x => x.Nombre == Carrera.Replace("-", " "));
+			if (datos != null)
+			{
+				vm = new MapaCurricularViewModel()
+				{
+					TotalCreditos = datos.Materias.Sum(x => x.Creditos),
+					Carreras = datos
+				};
+			}
+			if (datos == null)
+			{
+				return RedirectToAction("Index");
+			}
+
+			return View(vm);
+		}
+
+	}
 }
---
+++ PracticaU2OfertaAcademica/Models/ViewModels/MapaCurricularViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel;
+
+namespace PracticaU2OfertaAcademica.Models.ViewModels
+{
+	public class MapaCurricularViewModel
+	{
+		public int TotalCreditos { get; set; }
+		public Carreras Carreras { get; set; }
+	}
+}
